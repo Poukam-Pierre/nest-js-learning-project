d3ab8c03173a9d2e5d02a1176e5eea5538ccfedb
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-31 21:18:59 +0100

    feat(app):#7 exporting database service from here.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 
 @Module({
-  providers: [DatabaseService]
+  providers: [DatabaseService],
+  exports: [DatabaseService],
 })
 export class DatabaseModule {}
